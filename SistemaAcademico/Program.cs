93d9dbeaed43dc974f5debf547e451f4ab7ebf18
Eduardo <34611956+Eduualan@users.noreply.github.com>
2026-06-03 12:16:21 -0300

    Adicionando novos post e get matriculas

--- SistemaAcademico/Program.cs
+++ SistemaAcademico/Program.cs
@@ -124,4 +124,59 @@ app.MapDelete("/cursos/{id}", (int id, AppDbContext context) =>
     return Results.Ok("Curso removido");
 });
 
+// ==================== MATRÍCULAS ====================
+
+app.MapGet("/matriculas", (AppDbContext context) =>
+{
+    return context.Matriculas
+        .Include(m => m.Aluno)
+        .Include(m => m.Curso)
+        .ToList();
+});
+
+app.MapGet("/matriculas/{id}", (int id, AppDbContext context) =>
+{
+    var matricula = context.Matriculas
+        .Include(m => m.Aluno)
+        .Include(m => m.Curso)
+        .FirstOrDefault(m => m.Id == id);
+
+    if (matricula == null)
+        return Results.NotFound("Matrícula não encontrada");
+    return Results.Ok(matricula);
+});
+
+app.MapPost("/matriculas", (Matricula matricula, AppDbContext context) =>
+{
+    context.Matriculas.Add(matricula);
+    context.SaveChanges();
+    return Results.Created($"/matriculas/{matricula.Id}", matricula);
+});
+
+app.MapPut("/matriculas/{id}", (int id, Matricula atualizado, AppDbContext context) =>
+{
+    var matricula = context.Matriculas.Find(id);
+    if (matricula == null)
+        return Results.NotFound("Matrícula não encontrada");
+
+    matricula.AlunoId = atualizado.AlunoId;
+    matricula.CursoId = atualizado.CursoId;
+    matricula.DataMatricula = atualizado.DataMatricula;
+    matricula.Status = atualizado.Status;
+
+    context.SaveChanges();
+    return Results.Ok(matricula);
+});
+
+app.MapDelete("/matriculas/{id}", (int id, AppDbContext context) =>
+{
+    var matricula = context.Matriculas.Find(id);
+    if (matricula == null)
+        return Results.NotFound("Matrícula não encontrada");
+
+    context.Matriculas.Remove(matricula);
+    context.SaveChanges();
+    return Results.Ok("Matrícula removida");
+});
+
 app.Run();
