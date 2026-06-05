242179b6deebca32aea2974af856b6fd3758d6fb
Eduardo <34611956+Eduualan@users.noreply.github.com>
2026-06-05 13:37:20 -0300

    arrumando com responde

--- SistemaAcademico/models/Curso.cs
+++ SistemaAcademico/models/Curso.cs
@@ -3,10 +3,9 @@ namespace SistemaAcademico.Models;
 public class Curso
 {
     public int Id { get; set; }
-
     public string Nome { get; set; } = string.Empty;
-
     public string Professor { get; set; } = string.Empty;
-
     public int CargaHoraria { get; set; }
+
+    public List<Matricula> Matriculas { get; set; } = new();
 }
