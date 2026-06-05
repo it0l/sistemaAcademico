005c3994847097eb9ddfe77807bba4c77301a486
Eduardo <34611956+Eduualan@users.noreply.github.com>
2026-06-05 13:36:58 -0300

    Update Matricula.cs

--- SistemaAcademico/models/Matricula.cs
+++ SistemaAcademico/models/Matricula.cs
@@ -5,12 +5,11 @@ public class Matricula
     public int Id { get; set; }
 
     public int AlunoId { get; set; }
-    public Aluno Aluno { get; set; } = null!;
+    public Aluno? Aluno { get; set; }
 
     public int CursoId { get; set; }
-    public Curso Curso { get; set; } = null!;
+    public Curso? Curso { get; set; }
 
     public DateTime DataMatricula { get; set; }
-
     public string Status { get; set; } = string.Empty;
 }
