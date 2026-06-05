df5763cc98c7b17df88c1ddf21549ecd6ecdd8f6
Eduardo <34611956+Eduualan@users.noreply.github.com>
2026-06-05 13:37:45 -0300

    response add

--- SistemaAcademico/models/Aluno.cs
+++ SistemaAcademico/models/Aluno.cs
@@ -3,12 +3,10 @@ namespace SistemaAcademico.Models;
 public class Aluno
 {
     public int Id { get; set; }
-
     public string Nome { get; set; } = string.Empty;
-
     public string Email { get; set; } = string.Empty;
-
     public string MatriculaNumero { get; set; } = string.Empty;
-
     public DateTime DataNascimento { get; set; }
+
+    public List<Matricula> Matriculas { get; set; } = new();
 }
